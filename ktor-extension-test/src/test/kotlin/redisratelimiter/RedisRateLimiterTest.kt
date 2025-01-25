bbc3b13e2324ac0db702d2d51e189d0f758483e6
lolmageap <ekxk1234@naver.com>
2025-01-25 23:00:38 +0900

    add another test cases

--- ktor-extension-test/src/test/kotlin/redisratelimiter/RedisRateLimiterTest.kt
+++ ktor-extension-test/src/test/kotlin/redisratelimiter/RedisRateLimiterTest.kt
@@ -1,7 +1,9 @@
 package redisratelimiter
 
+import extension.ktor.redis.RateLimitExceededException
 import extension.ktor.redis.RedissonClientHolder
 import extension.ktor.redis.rateLimiter
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.lettuce.core.RedisClient
@@ -9,6 +11,7 @@ import io.lettuce.core.RedisURI
 import io.lettuce.core.api.async.RedisAsyncCommands
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
 import org.redisson.Redisson
 import org.redisson.config.Config
 import java.util.concurrent.atomic.AtomicInteger
@@ -53,5 +56,84 @@ class RedisRateLimiterTest : StringSpec({
         }.awaitAll()
 
         number.get() shouldBe 10
+    }
+
+    "rate limiter throws RateLimitExceededException" {
+        val key = "rate-limiter-with-exception"
+        val number = AtomicInteger(0)
+
+        (1..10).map {
+            async {
+                rateLimiter(key, 10, 1.seconds) {
+                    number.incrementAndGet()
+                }
+            }
+        }.awaitAll()
+
+        number.get() shouldBe 10
+
+        shouldThrow<RateLimitExceededException> {
+            rateLimiter(key, 10, 1.seconds) {
+                number.incrementAndGet()
+            }
+        }
+
+        number.get() shouldBe 10
+    }
+
+    "rate limiter test with different key" {
+        val lockedKey = "locked-key"
+        val unlockedKey = "unlocked-key"
+
+        val number = AtomicInteger(0)
+
+        (1..50).map {
+            async {
+                runCatching {
+                    rateLimiter(lockedKey, 10, 1.seconds) {
+                        number.incrementAndGet()
+                    }
+                }
+            }
+        }.awaitAll()
+
+        number.get() shouldBe 10
+
+        rateLimiter(unlockedKey, 10, 1.seconds) {
+            number.incrementAndGet()
+        }
+
+        number.get() shouldBe 11
+    }
+
+    "rate limiter test with end of period" {
+        val key = "rate-limiter-end-of-period"
+        val number = AtomicInteger(0)
+
+        (1..50).map {
+            async {
+                runCatching {
+                    rateLimiter(key, 10, 1.seconds) {
+                        number.incrementAndGet()
+                    }
+                }
+            }
+        }.awaitAll()
+
+        number.get() shouldBe 10
+
+        shouldThrow<RateLimitExceededException> {
+            rateLimiter(key, 10, 1.seconds) {
+                number.incrementAndGet()
+            }
+        }
+
+        delay(1.seconds)
+
+        rateLimiter(key, 10, 1.seconds) {
+            number.incrementAndGet()
+        }
+
+        number.get() shouldBe 11
     }
 })
